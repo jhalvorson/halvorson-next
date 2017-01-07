890ed471debf1cb4055a185d8c272ebd4b0009e8
Jamie Halvorson <jamie@halvorson.co.uk>
2017-01-07 11:39:07 +0000

    ✌🏻

---
+++ README.md
@@ -0,0 +1,3 @@
+# New front end for halvorson.co.uk
+
+Full write up will be happening 💩
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Index extends Component {
+  render() {
+    return <main>
+      <h1>Look, ma! I made a thing.</h1>
+    </main>
+  }
+}
